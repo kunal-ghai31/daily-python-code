2ace7e403c3a32ec059b39ef4210c2c95d102b29
Kunal Ghai <kunalghai.18@gmail.com>
2026-05-20 15:58:02 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,19 @@
+bs = float(input("Enter Basic Salary : "))
+hra = bs*0.20
+da = bs*0.30
+grs = bs+hra+da
+print("Gross Salary :",grs)
+a = 10
+a += 1
+a *= 2
+print(a)
+
+
+b = 9
+c = 88
+print (c>b)
+print (c<b)
+print (c>=b)
+print (c<=b)
+print (c!=b)
+print (c==b)
