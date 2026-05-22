4f5598e16693464659a49b74846f7aa00c5bbb61
Kunal Ghai <kunalghai.18@gmail.com>
2026-05-22 16:13:49 +0530

    Add files via upload

---
+++ and&or.py
@@ -0,0 +1,22 @@
+''' #or
+print(10>5 or 20>9)
+print(10>5 or 20>90)
+print(10>50 or 20>90)
+'''
+ 
+''' #and
+print(10>5 and 20>9)
+print(10>5 and 20>90)
+print(10>50 and 20>90)
+print ((10>5) + (20>30))
+a = 2
+b= 5
+print ( a and b) # last value will be the answer if no zero is found
+print ( b and a)
+print ( b or a) # first value will be the answer if no 
+'''
+a = 21
+b = 28
+print ( a & b) # and operator symbol
+print ( a | b) # or operator symbol
+
