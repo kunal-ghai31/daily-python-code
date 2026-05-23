4a1f4a1c92cb398810e3f986a0bf9bd1240aeb97
Kunal Ghai <kunalghai.18@gmail.com>
2026-05-23 16:22:40 +0530

    Add files via upload

---
+++ second exercise.py
@@ -0,0 +1,83 @@
+"""
+'1'
+a = 5
+b = 10
+print(a<b<<20) 
+
+'2'
+x = True
+y = False
+print(x+y*x)
+
+'3'
+print (4**0**2)
+
+'4'
+a = 12
+b = 5
+print(a^b)
+
+'5'
+print((2 and 0) or (0 and 3))
+
+'6'
+print(256 is 256)
+print(257 is 257)
+
+'7'
+a = 7
+print(~a + 1)
+
+'8'
+a = True
+b = False
+print((a or b) and not (a and b))
+
+'9'
+print(10 > 5 == True) 
+
+'10'
+print(2 + 3 * 4 ** 2 // 8 % 3) 
+
+'11'
+print(1 << 2 + 1)
+
+'12'
+a = 3
+b= 2
+a *= b+1
+print(a)
+
+'13'
+print(3 < 5 > 2 == 2) 
+
+'14'
+print(10 // 3 * 3 + 1 % 3)
+
+'15'
+x = 10
+y = 20
+print(x & y | x ^ y)
+
+'16'
+a = True
+b = False
+print(a & b or a ^ b) 
+
+'17'
+x = 8
+print(x >> 1 << 2)
+
+'18'
+print(5 + True * False + not False)
+
+'19'
+print((not 0) * (False or 1))
+
+'20'
+print(4 + 3 * 2 ** 2 // 2 - 1) 
+"""
+
+
+
+
